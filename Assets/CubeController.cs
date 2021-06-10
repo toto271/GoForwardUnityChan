2f5aa21ecc6b739ca6ecf540e44a6d62214589d4
toto271 <82321634+toto271@users.noreply.github.com>
2021-06-10 22:07:44 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private float speed = -12;
+    private float deadLine = -10;
+    public AudioClip sound;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag != "UnityChanTag")
+        {
+            audioSource.PlayOneShot(sound);
+        }
+    }
+}
